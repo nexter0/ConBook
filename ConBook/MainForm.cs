7a5011556d6ae56f310f69343718e6ddba666a00
nexter0 <nikodem.przybyszewski@gmail.com>
2023-07-11 18:18:22 +0200

    alternative safer saving method added

--- ConBook/MainForm.cs
+++ ConBook/MainForm.cs
@@ -7,7 +7,10 @@ namespace ConBook
 {
     public partial class MainForm : Form
     {
-        public BindingList<Contact> contacts = new();
+        public BindingList<Contact> contacts = new BindingList<Contact>();
+
+        private string? currentFile = null;
+        private bool currentFileEdited = false;
 
         public MainForm()
         {
@@ -54,27 +57,71 @@ namespace ConBook
             if (Regex.IsMatch(nameTextBox.Text, patternDigit)) { return 3; };
             if (Regex.IsMatch(surnameTextBox.Text, patternDigit)) { return 3; };
 
-
             return 0;
         }
 
         private void LoadFile(string fileName)
         {
-            XmlSerializer serializer = new(typeof(BindingList<Contact>));
+            XmlSerializer serializer = new XmlSerializer(typeof(BindingList<Contact>));
 
-            using FileStream fileStream = new(fileName, FileMode.Open);
-            BindingList<Contact> loadedContacts = (BindingList<Contact>)serializer.Deserialize(fileStream);
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Open))
+            {
+                BindingList<Contact> loadedContacts = (BindingList<Contact>)serializer.Deserialize(fileStream);
 
-            contacts.Clear();
-            contacts = new BindingList<Contact>(loadedContacts);
+                contacts.Clear();
+                contacts = new BindingList<Contact>(loadedContacts);
+            }
         }
+
+        private void SaveToNewFile(string fileName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(BindingList<Contact>));
+
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Create))
+            {
+                serializer.Serialize(fileStream, contacts);
+
+                if (currentFile == null)
+                {
+                    currentFile = fileName;
+                }
+            }
+        }
+
+        // This method is to prevent data loss during saving to an existing file
+        // in case of an exception
+        private void SaveToExistingFile(string fileName)
+        {
+            string tempFileName = Path.GetTempFileName();
+
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(BindingList<Contact>));
+
+                using (FileStream fileStream = new FileStream(tempFileName, FileMode.Create))
+                {
+                    serializer.Serialize(fileStream, contacts);
+                }
+                // File.Replace(tempFileName, fileName, null);
+                File.Delete(fileName);
+                File.Move(tempFileName, fileName);
+            }
+            finally
+            {
+                if (File.Exists(tempFileName))
+                {
+                    File.Delete(tempFileName);
+                }
+            }
+        }
+
 
         private void submitButton_Click(object sender, EventArgs e)
         {
             int validation = ValidateTextBoxes();
             if (validation == 0)
             {
-                Contact newContact = new(nameTextBox.Text, surnameTextBox.Text, phoneTextBox.Text);
+                Contact newContact = new Contact(nameTextBox.Text, surnameTextBox.Text, phoneTextBox.Text);
                 contacts.Add(newContact);
 
                 nameTextBox.Text = string.Empty;
@@ -99,36 +146,38 @@ namespace ConBook
 
         private void sortujToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            List<Contact> tempContactList = new(contacts);
-            tempContactList.Sort();
-            contacts = new BindingList<Contact>(tempContactList);
-            RefreshDataGridView();
+            if (contacts.Count > 0)
+            {
+                List<Contact> tempContactList = new List<Contact>(contacts);
+                tempContactList.Sort();
+                contacts = new BindingList<Contact>(tempContactList);
+                RefreshDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("Lista jest pusta.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
         }
 
         private void zapiszToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            // to do
-        }
-
-        private void zapiszJakoToolStripMenuItem_Click(object sender, EventArgs e)
-        {
             try
             {
-                SaveFileDialog saveFileDialog = new()
+                if (contacts.Count > 0)
                 {
-                    Filter = "Dokument XML (*.xml)|*.xml",
-                    Title = "Zapisz jako..."
-                };
-
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    if (currentFile != null)
+                    {
+                        SaveToExistingFile(currentFile);
+                    }
+                    else
+                    {
+                        otwórzToolStripMenuItem_Click(sender, e);
+                    }
+                }
+                else
                 {
-                    string fileName = saveFileDialog.FileName;
-
-                    XmlSerializer serializer = new(typeof(BindingList<Contact>));
-
-                    using FileStream fileStream = new(fileName, FileMode.Create);
-                    serializer.Serialize(fileStream, contacts);
-
+                    MessageBox.Show("Nie można zapisać pustej listy.", "Błąd zapisu", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
             catch (Exception ex)
@@ -148,13 +197,57 @@ namespace ConBook
                     MessageBox.Show(message, "Błąd zapisu", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
+            
+        }
+
+        private void zapiszJakoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (contacts.Count > 0) 
+            {
+                try
+                {
+                    SaveFileDialog saveFileDialog = new SaveFileDialog()
+                    {
+                        Filter = "Dokument XML (*.xml)|*.xml",
+                        Title = "Zapisz jako..."
+                    };
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string fileName = saveFileDialog.FileName;
+                        SaveToNewFile(fileName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (ex.InnerException != null)
+                    {
+                        Exception innerException = ex.InnerException;
+
+                        string message = "Błąd: \n" + innerException.Message + "\n"
+                            + "InnerException StackTrace: \n" + innerException.StackTrace;
+                        MessageBox.Show(message, "Błąd zapisu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        string message = "Błąd: \n" + ex.Message + "\n"
+                            + "StackTrace: \n" + ex.StackTrace;
+                        MessageBox.Show(message, "Błąd zapisu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nie można zapisać pustej listy.", "Błąd zapisu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
         }
 
         private void otwórzToolStripMenuItem_Click(object sender, EventArgs e)
         {
             try
             {
-                OpenFileDialog openFileDialog = new()
+                OpenFileDialog openFileDialog = new OpenFileDialog()
                 {
                     Filter = "Dokument XML (*.xml)|*.xml",
                     Title = "Otwórz..."
@@ -166,9 +259,9 @@ namespace ConBook
                     string fileName = openFileDialog.FileName;
 
                     LoadFile(fileName);
+                    currentFile = fileName;
                     RefreshDataGridView();
                 }
-
             }
             catch (Exception ex)
             {
@@ -179,16 +272,19 @@ namespace ConBook
         private void Form1_Load(object sender, EventArgs e)
         {
             string path = Directory.GetCurrentDirectory();
-            var file = Directory.EnumerateFiles(path, "*.xml").FirstOrDefault();
+            string? file = Directory.EnumerateFiles(path, "*.xml").FirstOrDefault();
             if (file != null) 
             {
                 try
                 {
                     LoadFile(file);
+                    currentFile = file;
+                    MessageBox.Show(currentFile);
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show("Podczas wczytywania wystąpił błąd: \n" + ex.Message + "\n\nWczytywany plik: " + file, "Błąd wczytywania", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Podczas wczytywania wystąpił błąd: \n" + ex.Message + "\n\nWczytywany plik: " + file, "Błąd wczytywania",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
 
